01dd33665a3c19fb523859d78801499cb7b5e0ef
Michlww222 <56195291+Michlww222@users.noreply.github.com>
2021-05-03 19:58:49 +0200

    Add files via upload

---
+++ Cell.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Cell {
+    public Line line;
+
+    public ArrayList<Cell> neighbors =  new ArrayList<>();
+
+
+    public Cell(Line line){
+        this.line = line;
+    }
+}
