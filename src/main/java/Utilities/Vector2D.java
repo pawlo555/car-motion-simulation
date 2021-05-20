b48fd1ec860f16c54f92d4e84f1a6702f286adbf
pawlo555 <spawel555@outlook.com>
2021-05-20 09:42:10 +0200

    Vector merge

--- src/main/java/Utilities/Vector2D.java
+++ src/main/java/Utilities/Vector2D.java
@@ -2,13 +2,14 @@ package Utilities;
 
 import java.util.Objects;
 
-public class Vector2D {
-    private final double horizontal;
-    private final double vertical;
 
-    public Vector2D(double horizontal, double vertical) {
-        this.horizontal = horizontal;
-        this.vertical = vertical;
+public class Vector2D {
+    public final double x;
+    public final double y;
+
+    public Vector2D(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
     @Override
@@ -16,24 +17,42 @@ public class Vector2D {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Vector2D vector2D = (Vector2D) o;
-        return horizontal == vector2D.horizontal && vertical == vector2D.vertical;
+        return x == vector2D.x && y == vector2D.y;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(horizontal, vertical);
+        return Objects.hash(x, y);
     }
 
     public boolean between(Vector2D upperLeft, Vector2D lowerRight) {
-        return (upperLeft.vertical <= this.vertical && this.vertical <= lowerRight.vertical)
-                && (upperLeft.horizontal <= this.horizontal && this.horizontal <= lowerRight.horizontal);
+        return (upperLeft.y <= this.y && this.y <= lowerRight.y)
+                && (upperLeft.x <= this.x && this.x <= lowerRight.x);
     }
 
-    public double getHorizontal() {
-        return horizontal;
+    public double getX() {
+        return x;
     }
 
-    public double getVertical() {
-        return vertical;
+    public double getY() {
+        return y;
     }
+
+    public double distance(Vector2D vector){
+        return Math.sqrt((x- vector.x)*(x- vector.x)+(y- vector.y)*(y- vector.y));
+    }
+
+    public Vector2D add(Vector2D vector){
+        return new Vector2D(x+vector.x, y+ vector.y);
+    }
+
+    public Vector2D multiply_scalar(double s){
+        return new Vector2D(x*s, y*s);
+    }
+
+    @Override
+    public String toString() {
+        return "("+ Math.round(x)+"," + Math.round(y) + "),";
+    }
+
 }
