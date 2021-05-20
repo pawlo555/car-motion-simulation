2209092b05aec04a706992b2e72fdac4f8fe092f
pawlo555 <spawel555@outlook.com>
2021-05-20 09:33:56 +0200

    vector2D merge

--- src/main/java/Utilities/Vector2d.java
+++ src/main/java/Utilities/Vector2d.java
@@ -1,46 +1,39 @@
 package Utilities;
 
-import Objects.Utils.RoundUtils;
-
 import java.util.Objects;
 
-public class Vector2d {
-    public double x;
-    public double y;
+public class Vector2D {
+    private final double horizontal;
+    private final double vertical;
 
-    public Vector2d(double x, double y){
-        this.x = x;
-        this.y = y;
-    }
-
-    public double distance(Vector2d vector){
-        return Math.sqrt((x- vector.x)*(x- vector.x)+(y- vector.y)*(y- vector.y));
-    }
-
-    public Vector2d add(Vector2d vector){
-        return new Vector2d(x+vector.x, y+ vector.y);
-    }
-
-    public Vector2d multiply_scalar(double s){
-        return new Vector2d(x*s, y*s);
-    }
-
-    @Override
-    public String toString() {
-        return "("+ RoundUtils.round(x,2)+"," + RoundUtils.round(y,2) + "),";
+    public Vector2D(double horizontal, double vertical) {
+        this.horizontal = horizontal;
+        this.vertical = vertical;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Vector2d vector2d = (Vector2d) o;
-        return Double.compare(vector2d.x, x) == 0 &&
-                Double.compare(vector2d.y, y) == 0;
+        Vector2D vector2D = (Vector2D) o;
+        return horizontal == vector2D.horizontal && vertical == vector2D.vertical;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(x, y);
+        return Objects.hash(horizontal, vertical);
+    }
+
+    public boolean between(Vector2D upperLeft, Vector2D lowerRight) {
+        return (upperLeft.vertical <= this.vertical && this.vertical <= lowerRight.vertical)
+                && (upperLeft.horizontal <= this.horizontal && this.horizontal <= lowerRight.horizontal);
+    }
+
+    public double getHorizontal() {
+        return horizontal;
+    }
+
+    public double getVertical() {
+        return vertical;
     }
 }
