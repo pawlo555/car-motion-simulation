2c4a01433f9aadbdcb6f6b9af7a532d4c37a55ac
mkroczek <1marcin100@gmail.com>
2021-05-22 20:36:45 +0200

    toString in Vector2D changed, Geometry folder deleted

--- src/main/java/Utilities/Vector2D.java
+++ src/main/java/Utilities/Vector2D.java
@@ -52,7 +52,7 @@ public class Vector2D {
 
     @Override
     public String toString() {
-        return "("+ Math.round(x)+"," + Math.round(y) + "),";
+        return "("+ Math.round(x)+"," + Math.round(y) + ")";
     }
 
 }
